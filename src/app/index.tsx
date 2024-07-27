1c5f0bf800088842d181830d4cf8fb939919c0fa
jungggggggg <jungsu0705@naver.com>
2024-07-27 23:02:43 +0900

    rebuild a structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,55 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, Image } from "react-native";
-import GetWeather from "../component/WeatherAPI";
+import { Redirect } from "expo-router";
 
-type weatherDataType = {
-    weather:
-    {
-        main: string;
-        icon: string
-    },
-    main: {
-        temp: number;
-    }
-}
-
-export default function Main() {
-
-    const [data, setData] = useState<weatherDataType>();
-    const [loading, setLoading] = useState(true);
-
-    const fetchWeatherData = async () => {
-        try {
-            const weatherData = await GetWeather();
-            setData(weatherData);
-        }
-        catch (error) {
-            return (error);
-        }
-        finally {
-            setLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        fetchWeatherData();
-    }, [])
-
-    return (
-        <View style={{ flex: 1 }}>
-            {loading ?
-                (<Text>Loading.....</Text>)
-                :
-                (
-                    <View>
-                        <Text>{data.main.temp}</Text>
-                        <Image
-                            source={{ uri: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` }}
-                            style={{ width: 100, height: 100 }}
-                        />
-                    </View>
-                )
-            }
-        </View>
+export default function GoToScreen() {
+    return(
+        <Redirect href='./(tabs)/WeatherList'/>
     )
 }
--- src/component/WeatherAPI.tsx
+++ src/component/WeatherAPI.tsx
@@ -3,10 +3,9 @@ const API_KEY = process.env.OPENWEATHER_API_KEY
 
 
 const GetWeather = async () => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.5665&lon=126.9780&appid=${API_KEY}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&lang=eng&units=metric&appid=${API_KEY}`)
     const data = await response.json()
 
-    console.log(data)
     return data;
 }
 
